60f7d843024970808a03898f865e0d33740399af
kamal kant agarwal <kamalmittal92@gmail.com>
2017-05-30 14:48:06 +0530

    Update DevViewController.swift

--- GitTestProject/DevViewController.swift
+++ GitTestProject/DevViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class DevViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad();print("DevVC");
+        super.viewDidLoad();print("DevVC1");
     }
 
     override func didReceiveMemoryWarning() {
